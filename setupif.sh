0009b531a70c8562e6f777a1b73c71323c8a08ed
Clemens Boennen <clemens.boennen@rwth-aachen.de>
2021-01-04 21:17:04 +0100

    added bridge support for fast dds

--- setupif.sh
+++ setupif.sh
@@ -4,8 +4,11 @@ sudo ip tuntap add dev tap0 mode tap
 sudo ip tuntap add dev tap1 mode tap
 sudo brctl addbr br0
 sudo brctl addif br0 tap0 tap1
+sudo ifconfig tap0 0.0.0.0
+sudo ifconfig tap1 0.0.0.0
 sudo ifconfig tap0 up
 sudo ifconfig tap1 up
+sudo ifconfig br0 192.168.1.1
 sudo ifconfig br0 up
 
 #setup in ~/.bashrc
